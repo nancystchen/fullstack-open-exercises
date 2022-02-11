bd3c4fda82a5c16c955b6194eb2be11b97e4465b
nancystchen <nanstchen@gmail.com>
2022-02-10 23:04:25 -0800

    part1 c

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => {
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
   const name = 'Peter';
   const age = 30;
-  console.log("Hello from component");
+  const increment_counter = () => setCounter(counter + 1)
+  const decrement_counter = () => setCounter(counter - 1)
+  const reset_counter = () => setCounter(0)
   return (
     <>
       <h1>Greetings</h1>
+      <Display counter={counter} />
       <Hello name="Gregor" age={11+22}/>
       <Hello name={name} age={age}/>
+      <Button 
+        onClick={increment_counter}
+        text="increase count" 
+      />
+      <Button 
+        onClick={decrement_counter}
+        text="decrease count"
+      />
+      <Button 
+        onClick={reset_counter} 
+        text="reset count"
+      />
     </>
   );
 }
 
-const Hello = (props) => {
+const Display = ({counter}) => {
+  return(
+    <div> {counter} </div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}> 
+      {text}
+    </button>
+  )
+}
+
+const Hello = ({name, age}) => {
   return (
     <div>
-      <p>Hello {props.name} {props.age}</p>
+      <p>Hello {name} {age}</p>
     </div>
   );
 }

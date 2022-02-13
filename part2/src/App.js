2cfb40272b3c525f54c2e14ccf3e9411ddb1472a
nancystchen <nanstchen@gmail.com>
2022-02-13 13:21:39 -0800

    part2.b

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+
+  const getNotes = () => 
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+
+  useEffect(getNotes,[])
 
   const addNote = (event) => {
     event.preventDefault()

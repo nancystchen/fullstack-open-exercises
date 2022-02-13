c3ef88fcd34e87bb3833e0e5ee7116293e33fecd
nancystchen <nanstchen@gmail.com>
2022-02-12 16:25:47 -0800

    setup part2

---
+++ part2/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import Note from './components/Note';
+
+const App = ({notes}) => {
+  return (
+    <div>
+      <h1> Notes </h1>
+      <ul>
+        {notes.map(note => 
+          <Note note={note} key={note.id} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
